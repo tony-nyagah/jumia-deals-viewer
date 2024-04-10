0bf8422e7b51e4e0168db832bcce9fd4c26b634f
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-04-10 13:20:35 +0300

    Save scraped data to an sqlte db

--- get_scraper_data.py
+++ get_scraper_data.py
@@ -3,13 +3,11 @@ from scrapy.crawler import CrawlerProcess
 from jumiaScraper.spiders.deals_spider import DealsSpider
 
 
-def run_spider_and_export_json():
-    process = CrawlerProcess(
-        settings={"FEEDS": {"deals.jsonl": {"format": "jsonlines", "overwrite": True}}}
-    )
+def run_spider():
+    process = CrawlerProcess()
     process.crawl(DealsSpider)
     process.start()
 
 
 if __name__ == "__main__":
-    run_spider_and_export_json()
+    run_spider()
--- jumiaScraper/pipelines.py
+++ jumiaScraper/pipelines.py
@@ -4,18 +4,55 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+import sqlite3
+
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class JumiascraperPipeline:
-    def process_item(self, item, spider):
-        return item
-
-
-class SqlitePipeline:
     def __init__(self):
-        pass
+        # create/connect to a database
+        self.connection = sqlite3.connect("deals.sqlite")
+
+        # create cursor, used to execute commands
+        self.cursor = self.connection.cursor()
+
+        # create quotes table if none exists
+        self.cursor.execute(
+            """
+        CREATE TABLE IF NOT EXISTS deals(
+            product_name TEXT,
+            product_price TEXT,
+            old_price TEXT,
+            product_link TEXT        
+        )
+        """
+        )
 
     def process_item(self, item, spider):
+
+        # check to see if item  is already in the db
+        self.cursor.execute(
+            "SELECT * FROM deals WHERE product_link = ?", (item["product_link"],)
+        )
+        result = self.cursor.fetchone()
+
+        # if it is in the db, create a log message
+        if result:
+            spider.logger.warn(f"Product already in database: {item['product_link']}")
+        else:
+            # define insert statement
+            self.cursor.execute(
+                """INSERT INTO deals (product_name, product_price, old_price, product_link) VALUES (?, ?, ?, ?)""",
+                (
+                    item["product_name"],
+                    item["product_price"],
+                    item["old_price"],
+                    item["product_link"],
+                ),
+            )
+            # insert data into the database
+            self.connection.commit()
+
         return item
