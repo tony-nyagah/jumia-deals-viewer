63b9c9f2bb0fb9f375f6c67bbd99505386b9aa6b
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-07-18 23:58:05 +0300

    add product links and pagination

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import click
 import sqlite3
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 
 from jinja2 import StrictUndefined
@@ -14,12 +14,7 @@ app.jinja_env.undefined = StrictUndefined
 app.register_blueprint(get_deals.bp)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html.j2")
-
-
-@app.route("/api/deals")
+# @app.route("/api/deals")
 def get_deals():
     connection = sqlite3.connect("deals.db")
     cursor = connection.cursor()
@@ -39,6 +34,27 @@ def get_deals():
         deals_list.append(deal_dict)
 
     return jsonify(deals_list)
+
+
+@app.route("/")
+def index():
+    deals = get_deals()
+    deals_list = deals.get_json()
+
+    page = request.args.get("page", 1, type=int)
+    items_per_page = 12
+    total_pages = (len(deals_list) + items_per_page - 1) // items_per_page
+
+    if page < 1 or page > total_pages:
+        return "Invalid page number."
+
+    start_index = (page - 1) * items_per_page
+    end_index = start_index + items_per_page
+    paginated_deals = deals_list[start_index:end_index]
+
+    return render_template(
+        "index.html.j2", context=paginated_deals, page=page, total_pages=total_pages
+    )
 
 
 @click.command()
