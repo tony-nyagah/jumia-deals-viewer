02e0521ea3c9bdf66acc44d8714fb4e0dc3447f8
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-09-01 00:55:47 +0300

    new folder setup with rye

--- blueprints/get_deals.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bp = Blueprint("get_deals", __name__, url_prefix="/get_deals")
-
-
-@bp.route("/")
-def index():
-    return render_template("get_deals/index.html.j2")
--- tasks.md
+++
@@ -1,4 +0,0 @@
-Tasks to track project progress:
-
-- [x] Put the fetched data in a database.
-- [ ] Create a web app to display the data.
