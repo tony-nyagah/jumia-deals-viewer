5e74174ac04e5e912317f4fae18f1dedbbe324dd
Antony Nyagah <tony.m.nyagah@gmail.com>
2024-09-02 15:02:57 +0300

    update user agent in scrapy settings

--- src/jumia_deals_viewer/deal_scraper/settings.py
+++ src/jumia_deals_viewer/deal_scraper/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "deal_scraper.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "deal_scraper (+http://www.yourdomain.com)"
+USER_AGENT = "Jumia Deals Scraper by Mr. Nyagah"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
